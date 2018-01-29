147e433765c2bd7b96a4a5eb4e51b51a06c28e91
Kathy Hudson <khitx@icloud.com>
2018-01-29 21:34:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,7 @@ require "pry"
 
 class Author
   attr_accessor :name, :posts
+  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -9,13 +10,20 @@ class Author
   end
 
   def add_post(post)
+    @@post_count += 1
     @posts << post
     post.author = self
   end
 
   def add_post_by_title(title)
+    @@post_count += 1
     post = Post.new(title)
     @posts << post
     post.author = self
   end
+
+  def self.post_count
+    @@post_count
+  end
+
 end
