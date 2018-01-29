b077007d23ecdf222bcf4cde5f307edd0ec128b4
Kathy Hudson <khitx@icloud.com>
2018-01-29 21:30:59 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,16 @@
 class Post
-  attr_accessor :post, :title, :author
+  #attr_accessor :post, :title, :author
 
-  def initialize(post, title=nil)
-    @post = post
+  #def initialize(post, title=nil)
+  #  @post = post
+  #  @title = title
+  #end
+
+  attr_accessor :title, :author
+
+  def initialize(title=nil)
     @title = title
   end
+
 
 end
