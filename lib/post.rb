b04cf47f6fd1fa892062f219f6152a0f99f0cac4
Kathy Hudson <khitx@icloud.com>
2018-01-29 21:57:07 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -14,11 +14,11 @@ class Post
   end
 
   def author_name
-#    if self.author = nil
-#      nil
-#    else
+    if self.author == nil
+      nil
+    else
       self.author.name
-#    end
+   end
     #binding.pry
   end
 
